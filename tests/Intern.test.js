4fcf13fdc052edd7a1dc1c0fd5215271c3f27c48
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-08-27 09:40:16 -0600

    Intern Test Complete

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -4,7 +4,7 @@ describe('Intern', () => {
     let Obj = new Intern('ray','123','ray@ray.com',"DU");
 
     it('Initialize', () => {
-        expect(Obj).toEqual({"school": "ray@ray.com", "id": "123", "name": "ray","school": "DU"});
+        expect(Obj).toEqual({"email": "ray@ray.com", "id": "123", "name": "ray","school": "DU"});
     });
     it('gets School', () => {
         expect(Obj.getSchool()).toBe('DU');
