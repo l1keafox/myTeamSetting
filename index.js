95ff8334d9bba74b20aa8bcb21ed8dc1451b8ad0
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-08-27 09:59:39 -0600

    Init index.js, added userStory

---
+++ index.js
@@ -0,0 +1,42 @@
+// AS A manager
+// I WANT to generate a webpage that displays my team's basic info
+// SO THAT I have quick access to their emails and GitHub profiles
+
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for my team members and their information
+
+let inquire = require('inquirer');
+
+let managerQuestions = [
+
+];
+
+// WHEN I start the application
+// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+// WHEN I enter the team manager’s name, employee ID, email address, and office number
+
+
+// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+let menu = [
+
+];
+
+// WHEN I select the engineer option
+let engineerQuestions = [
+
+];
+// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+// WHEN I select the intern option
+let internQuestions = [
+
+];
+// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+// WHEN I decide to finish building my team
+
+
+// THEN I exit the application, and the HTML is generated
+// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
+// WHEN I click on an email address in the HTML
+// THEN my default email program opens and populates the TO field of the email with the address
+// WHEN I click on the GitHub username
+// THEN that GitHub profile opens in a new tab
